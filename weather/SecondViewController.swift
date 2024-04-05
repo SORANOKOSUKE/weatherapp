844a3c1e7662aa33fd4418ba2a018706f867c528
SORANOKOSUKE <104891461+SORANOKOSUKE@users.noreply.github.com>
2024-04-05 15:49:59 +0900

    commit

--- weather/SecondViewController.swift
+++ weather/SecondViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 import Foundation
 import os
-import Combine
 
 class SecondViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource,UITableViewDelegate,UITableViewDataSource {
 
